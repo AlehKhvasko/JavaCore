686a39e5af7f5b9872cde0521c7049af2d401348
AlehKhvasko <alehkhvasko@yahoo.com>
2022-03-10 17:17:02 -0600

    first commit JDBC creation using SQLite database

---
+++ JDBC/src/main/java/Repositoties/DBConnection.java
@@ -0,0 +1,19 @@
+package Repositoties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    public static Connection connect(){
+        Connection con = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            con = DriverManager.getConnection("jdbc:sqlite:StudentDB.db");
+            System.out.println("Connected");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
