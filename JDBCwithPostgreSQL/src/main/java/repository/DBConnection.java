90ef566b230ba396a4233c404739d27b5f16e432
AlehKhvasko <alehkhvasko@yahoo.com>
2022-03-16 22:29:38 -0500

    getting started with project

---
+++ JDBCwithPostgreSQL/src/main/java/repository/DBConnection.java
@@ -0,0 +1,12 @@
+package repository;
+import java.sql.Connection;
+
+public class DBConnection {
+    public Connection connect(){
+        Connection con = null;
+
+
+        return con;
+    }
+
+}
