cec8951c40ce7fc4a4290f5a7ba12518aa8fdaa1
AlehKhvasko <alehkhvasko@yahoo.com>
2022-03-21 22:55:27 -0500

    SQLite first connection to DB

--- JDBCwithPostgreSQL/src/main/java/repository/SQLiteRepositoryImpl.java
+++ JDBCwithPostgreSQL/src/main/java/repository/SQLiteRepositoryImpl.java
@@ -1,8 +1,6 @@
 package repository;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class SQLiteRepositoryImpl implements DBConnection {
 
@@ -10,9 +8,13 @@ public class SQLiteRepositoryImpl implements DBConnection {
     public Connection connect(String dbname, String user, String password) {
         Connection connection = null;
         try {
-            String url = "jdbc:sqlite:C:\\Users\\alehk\\OneDrive\\Desktop\\DummyDB\\WeatherDB.db";
+            String url = "jdbc:sqlite:C:\\Users\\alehk\\OneDrive\\Desktop\\DummyDB\\ApiWeather.db";
             connection = DriverManager.getConnection(url);
-            System.out.println("SQLite connection has been established.");
+            if (connection != null){
+                System.out.println("SQLite connection has been established.");
+            }else{
+                System.err.println("SQLite connection failed.");
+            }
         } catch (SQLException e) {
             System.out.println(e);
         }
@@ -26,13 +28,34 @@ public class SQLiteRepositoryImpl implements DBConnection {
 
     @Override
     public void insert(Connection con, String tableName, String city, String country, String cityid) {
-
+        Statement stmt = null;
+        String sql = "insert into top50cities (city, country, cityid) values (" + city + "," + country + "," + cityid + ")";
+        try {
+            stmt = con.createStatement();
+            stmt.executeUpdate(sql);
+            System.out.println("Data has been added.");
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
     }
 
 
     @Override
     public void read(Connection con, String tableName) {
+            String sql = "select * from " + tableName;
 
+            try (Statement st = con.createStatement();
+                 ResultSet rs = st.executeQuery(sql)){
+
+                while (rs.next()){
+                    System.out.println(rs.getString("empid") + " " +
+                            rs.getString("city") +" " +
+                            rs.getString("country") + " " +
+                            rs.getString("cityid") + "\n");
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
     }
 
     @Override
